93e5b42d868931e77a037ea27d0343bc4c2ec0ad
abdullahtes <abdullah.info88@gmail.com>
2022-12-10 04:45:29 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,4 +2,20 @@
 /**
  * main - Entry point
  *
- *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
